aacfcd078ccd5e05cd4f3190e86cb7a97ff70d5a
Ajay Pradhan <pradhanajay14@gmail.com>
2021-09-29 22:57:14 +0530

    create and save product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,18 @@
+const Product = require("../models/products");
+
+// create new products => /api/v1/product/new
+
+exports.newProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
 exports.getProducts = (req, res, next) => {
   res.status(200).json({
-    "success": true,
-    "message": "this route will show all products in database",
+    success: true,
+    message: "this route will show all products in database",
   });
 };
