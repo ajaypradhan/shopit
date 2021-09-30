cd9a864e7e836e4ed132fc967e18fcbded0c1370
Ajay Pradhan <pradhanajay14@gmail.com>
2021-09-30 18:11:08 +0530

    unhandled promises Rejection

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,17 @@ dotenv.config({ path: "backend/config/config.env" });
 // connecting to DATABASE
 connectDatabase();
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(
     `server started at PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`
   );
+});
+
+// handel unHandled Promise rejection
+process.on("unhandledRejection", (err) => {
+  console.log(`ERROR : ${err.message}`);
+  console.log("Shutting down server due to unhandledRejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
