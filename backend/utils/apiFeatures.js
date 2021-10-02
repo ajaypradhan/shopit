e85942845699e37ddfa19f91ca7a4a7efcfd0960
Ajay Pradhan <pradhanajay14@gmail.com>
2021-10-02 11:03:54 +0530

    filter product

--- backend/utils/apiFeatures.js
+++ backend/utils/apiFeatures.js
@@ -14,8 +14,26 @@ class APIFeatures {
           },
         }
       : {};
-    console.log(keyword);
+    // console.log(keyword);
     this.query = this.query.find({ ...keyword });
+    return this;
+  }
+
+  // filter method
+  filter() {
+    const queryCopy = { ...this.queryStr };
+
+    // removing fields from the query
+    const removeFields = ["keyword", "limit", "page"];
+    removeFields.forEach((el) => {
+      delete queryCopy[el];
+    });
+
+    // advance filter for price ,ratings  etc
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+
+    this.query = this.query.find(JSON.parse(queryStr));
     return this;
   }
 }
