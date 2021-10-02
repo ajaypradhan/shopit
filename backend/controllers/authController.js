c5f5550defc618316e7b09fda52b4462be3c30b0
Ajay Pradhan <pradhanajay14@gmail.com>
2021-10-02 23:38:38 +0530

    login user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -17,7 +17,38 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     },
   });
 
-  const token = user.getJwtToken()
+  const token = user.getJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
+
+// login user => /api/v1/login
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // check if email and password is enter by user
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email and password", 400));
+  }
+
+  //finding  user in database
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  // checks is password is correct or not
+  const isPasswordMatch = await user.comparePassword(password);
+
+  if (!isPasswordMatch) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  const token = user.getJwtToken();
 
   res.status(200).json({
     success: true,
