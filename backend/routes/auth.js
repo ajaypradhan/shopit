56ad8f16caeb441a7826fbbb9085590c46a07d07
Ajay Pradhan <pradhanajay14@gmail.com>
2021-10-03 21:49:48 +0530

    update password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -9,6 +9,7 @@ const {
   forgotPassword,
   resetPassword,
   getUserProfile,
+  updatePassword,
 } = require("../controllers/authController");
 
 const { isAuthenticatedUser, authorizeRoles } = require("../middlewares/auth");
@@ -22,5 +23,6 @@ router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logout);
 
 router.route("/me").get(isAuthenticatedUser, getUserProfile);
+router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 
 module.exports = router;
